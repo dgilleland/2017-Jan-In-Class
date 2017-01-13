1c0dd766d30113649642dec838dd95813a6eab69
Dan Gilleland <dgilleland@nait.ca>
2017-01-13 13:13:11 -0700

    In-class practice with functions and modifying elements

---
+++ Week 02/MoreFunctions/practice.js
@@ -0,0 +1,22 @@
+/* Practice with Functions and HTML Content */
+
+// 1) Create a function that will prompt the user for some text and then place that text inside the <pre> tag of the figure with the ID of "sample".
+//    (Be sure to select the target element in a way that identifies all aspects of the query: figure, ID, and pre)
+function displayText() { }
+
+// 2) Create a function that will change the background color of the pre tag to "aqua".
+//    (You will need to use the .setAttribute() method to set the style.)
+function aquaBackground() { }
+
+// 3) Create a function that will clear the background color of the pre tag (remove the style attribute).
+function clearBackground() { }
+
+// 4) Create a function that can be used to add the class 'col-reverse' to the body
+//    (You will need to research which element method you will have to use to set the class.)
+function changeMainClass(className) { }
+
+// 5) Create a function that will select all the paragraphs of the main element and return a count of the number of paragraphs.
+function countParagraphs() { }
+
+// 6) Create a function that will select the first paragraph of the main element and return the number of words in that paragraph.
+function countWords() { }
