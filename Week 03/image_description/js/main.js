80d7adc7e6daf2937d5d8db553b7bf54dde18e7f
Dan Gilleland <dgilleland@nait.ca>
2017-01-20 12:54:26 -0700

    Answers to in-class questions on event handling

--- Week 03/image_description/js/main.js
+++ Week 03/image_description/js/main.js
@@ -2,10 +2,12 @@
 
 // Step 1) Select the link
 // TODO: create a variable that holds a reference to the anchor tag with the class names "feature" and "link". Call that variable featureLink
+//  featureLink will reference an anchor tag
 var featureLink = document.querySelector('a.link.feature');
 
 // Step 2) Create the event listener function (aka event handler)
 function featureLinkHandler(evt) { //evt parameter is an object representing the event as it is passed to our method
+    //  featureImage will reference an image tag
     var featureImage = document.querySelector('img.feature');
     featureImage.src = featureLink.href;
     // All elements have a collection of class names that we can access
@@ -17,4 +19,6 @@ function featureLinkHandler(evt) { //evt parameter is an object representing the
 
 // Step 3) Add the function as an event listener (hook it up to the 'click' event)
 featureLink.addEventListener('click', featureLinkHandler);
+//                          \ event / \  method name   /
+//                           \name /
 // Note that the featureLinkHandler is not called right now. Instead, it will be called when the click event happens on the featureLink element.
