0ffeef573a86923800a268ee55d3b4e57f82ede5
Dan Gilleland <dgilleland@nait.ca>
2017-01-24 13:34:33 -0700

    Exercise

---
+++ Week 04/image_description/js/main.js
@@ -0,0 +1,33 @@
+// Note: The code here is presuming that this script is loaded at the END of the HTML Body tag.
+
+// Step 1) Select the link
+// TODO: create a variable that holds a reference to the anchor tag with the class names "feature" and "link". Call that variable featureLink
+//  featureLink will reference an anchor tag
+var featureLink = document.querySelector('a.link.feature');
+
+// Step 2) Create the event listener function (aka event handler)
+function featureLinkHandler(evt) { //evt parameter is an object representing the event as it is passed to our method
+    //  featureImage will reference an image tag
+    var featureImage = document.querySelector('img.feature');
+    if(featureImage.classList.contains('hidden')) {
+        // Show the image
+        featureImage.src = featureLink.href;
+        // All elements have a collection of class names that we can access
+        featureImage.classList.remove('hidden');
+        featureLink.innerHTML = featureLink.innerHTML.replace('View', 'Hide');
+    } else {
+        // Hide the image
+        featureImage.src = ''; // to remove the image from the DOM
+        featureImage.classList.add('hidden');
+        featureLink.innerHTML = featureLink.innerHTML.replace('Hide', 'View');
+    }
+
+    // Stop the normal event from happening
+    evt.preventDefault();
+}
+
+// Step 3) Add the function as an event listener (hook it up to the 'click' event)
+featureLink.addEventListener('click', featureLinkHandler);
+//                          \ event / \  method name   /
+//                           \name /
+// Note that the featureLinkHandler is not called right now. Instead, it will be called when the click event happens on the featureLink element.
