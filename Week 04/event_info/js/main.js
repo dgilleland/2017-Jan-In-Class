23a8e0a71061ea6f06f2b055d99d430c4a2c7c2d
Dan Gilleland <dgilleland@nait.ca>
2017-01-26 12:55:21 -0700

    Notes and variations on events demo

--- Week 04/event_info/js/main.js
+++ Week 04/event_info/js/main.js
@@ -1,3 +1,5 @@
+// This function is a generic function to just output the event information and
+// information about the object that is "holding" the function (this keyword).
 function about(evt) {
   console.log(evt);
   // TODO: Learn about the event object (type, target, srcElement, etc.)
@@ -7,18 +9,23 @@ function about(evt) {
 }
 
 function pageReady() {
-
+  // All images in the <header> will call the about() function for the click event
   var images = document.getElementsByTagName('img');
   for (var i = 0; i < images.length; i++) {
     if (images[i].parentElement.parentElement.tagName === 'HEADER') {
       images[i].addEventListener('click', about);
     }
   }
+
+  var hdr = document.querySelector('header');
+  hdr.addEventListener('click', about);
 }
 
-document.onreadystatechange = function () {
-  if (document.readyState === 'complete') {
-    // DOM is ready!
-    pageReady();
-  }
-};
+// document.onreadystatechange = function () {
+//   if (document.readyState === 'complete') {
+//     // DOM is ready!
+//     pageReady();
+//   }
+// };
+
+window.onload = pageReady;
