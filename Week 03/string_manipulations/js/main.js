80f0338cb9c064206855b2e11b0f447f2242699d
Dan Gilleland <dgilleland@nait.ca>
2017-02-02 13:37:07 -0700

    tweaks in code

--- Week 03/string_manipulations/js/main.js
+++ Week 03/string_manipulations/js/main.js
@@ -1,11 +1,12 @@
-const urlTemplate = 'http://placehold.it/:size/:color/:background/';
-const urlQueryStringTemplate = "?text="
+var urlTemplate = 'http://placehold.it/:size/:color/:background/';
+var urlQueryStringTemplate = "?text="
 
 // mini-lesson on string manipulation:
 
 //  split string into an array
 //  join from array into a string
 //  includes / endswith / startsWith
+//  match - regular expressions
 //  indexof / lastIndexOf
 //  replace
 //  character access
