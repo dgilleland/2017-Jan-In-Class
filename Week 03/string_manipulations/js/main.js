c13e2b0476e9b0ab4584f987432e84a46c601626
Dan Gilleland <dgilleland@nait.ca>
2017-02-02 11:56:39 -0700

    Ideas for images + string manipulations

---
+++ Week 03/string_manipulations/js/main.js
@@ -0,0 +1,16 @@
+const urlTemplate = 'http://placehold.it/:size/:color/:background/';
+const urlQueryStringTemplate = "?text="
+
+// mini-lesson on string manipulation:
+
+//  split string into an array
+//  join from array into a string
+//  includes / endswith / startsWith
+//  indexof / lastIndexOf
+//  replace
+//  character access
+//  comparing - equals, greater/lesser
+//  slice
+//  toString
+//  trim
+
