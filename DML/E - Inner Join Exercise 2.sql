e6904e207a5208cb44e825cd260db6badc56c24f
Dan Gilleland <dgilleland@nait.ca>
2017-03-01 14:23:43 -0700

    Join Exercise 2 questions

---
+++ DML/E - Inner Join Exercise 2.sql
@@ -0,0 +1,26 @@
+/*  Join Exercise 2
+    Inner Joins With Aggregates Exercises  */
+USE [A01-School]
+GO
+
+--1. How many staff are there in each position? Select the number and Position Description
+
+--2. Select the average mark for each course. Display the CourseName and the average mark. Sort the results by average in descending order.
+
+--3. How many payments where made for each payment type. Display the PaymentTypeDescription and the count
+ -- TODO: Student Answer Here...
+
+--4. Select the average Mark for each student. Display the Student Name and their average mark. Use table aliases in your FROM & JOIN clause.
+
+--5. Select the same data as question 4 but only show the student names and averages that are > 80
+ -- TODO: Student Answer Here...
+ 
+--6.what is the highest, lowest and average payment amount for each payment type Description? 
+ -- TODO: Student Answer Here...
+
+--7. How many students are there in each club? Show the clubName and the count
+ -- TODO: Student Answer Here...
+ 
+--8. Which clubs have 3 or more students in them? Display the Club Names.
+ -- TODO: Student Answer Here...
+ 
