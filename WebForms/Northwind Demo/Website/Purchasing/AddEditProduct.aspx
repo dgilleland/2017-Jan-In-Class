035714a74ee19f5d375bb7a4da24c300ffddbfcd
Dan Gilleland <dgilleland@nait.ca>
2017-03-13 13:46:22 -0600

    Add/Edit Products - List all products

---
+++ WebForms/Northwind Demo/Website/Purchasing/AddEditProduct.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="AddEditProduct.aspx.cs" Inherits="Purchasing_AddEditProduct" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <div class="row">
+        <h1 class="page-header">Add/Edit Products</h1>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-inline">
+                        <div class="form-group">
+                            <asp:Label ID="Label1" runat="server"
+                                 CssClass="control-label"
+                                 Text="Products"
+                                 AssociatedControlID="CurrentProducts"
+                                 />
+                            <asp:DropDownList ID="CurrentProducts"
+                                 runat="server"
+                                 CssClass="form-control">
+                            </asp:DropDownList>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 text-center"></div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ WebForms/Northwind Demo/Website/Purchasing/AddEditProduct.aspx.cs
@@ -0,0 +1,35 @@
+﻿using NorthwindEntities;    // for the appropriate Entity classes
+using NorthwindSystem.BLL;  // for the InventoryPurchasingController
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Purchasing_AddEditProduct : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack) // on the initial GET of the page
+        {
+            // We can populate some controls such as DropDownLists with data
+            // Populate CurrentProducts with all the products in the database
+            InventoryPurchasingController controller = new InventoryPurchasingController();
+            // controller is a NorthwindController object
+            List<Product> products = controller.ListAllProducts();
+            // products is a List<Product> object
+
+            CurrentProducts.DataSource = products;
+            // CurrentProducts is a DropDownList
+
+            CurrentProducts.DataTextField = "ProductName";
+            // The property of the Product class to display
+            CurrentProducts.DataValueField = "ProductID";
+            // The property of the Product class to use as the value
+
+            CurrentProducts.DataBind(); // Populate the DropDownList
+            CurrentProducts.Items.Insert(0, "[select a product]");
+        }
+    }
+}
