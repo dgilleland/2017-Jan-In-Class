9a494fba8140526fcae970c1220796b9634061fd
Dan Gilleland <dgilleland@nait.ca>
2017-03-16 09:54:20 -0600

    Show all product details

--- WebForms/Northwind Demo/Website/Purchasing/AddEditProduct.aspx
+++ WebForms/Northwind Demo/Website/Purchasing/AddEditProduct.aspx
@@ -41,8 +41,33 @@
 
                 <asp:Label ID="Label2" runat="server" Text="Product Name" AssociatedControlID="ProductName" />
                 <asp:TextBox ID="ProductName" runat="server" />
+
+                <asp:Label ID="Label4" runat="server" Text="Supplier" AssociatedControlID="Supplier"></asp:Label>
+                <asp:DropDownList ID="Supplier" runat="server"></asp:DropDownList>
+
+                <asp:Label ID="Label5" runat="server" Text="Category" AssociatedControlID="Category"></asp:Label>
+                <asp:DropDownList ID="Category" runat="server"></asp:DropDownList>
+                
+                <asp:Label ID="Label6" runat="server" Text="Qty per Unit" AssociatedControlID="QtyPerUnit"></asp:Label>
+                <asp:TextBox ID="QtyPerUnit" runat="server"></asp:TextBox>
+
+                <asp:Label ID="Label7" runat="server" Text="Unit Price" AssociatedControlID="UnitPrice"></asp:Label>
+                <asp:TextBox ID="UnitPrice" runat="server"></asp:TextBox>
+
+                <asp:Label ID="Label8" runat="server" Text="In Stock" AssociatedControlID="InStock"></asp:Label>
+                <asp:TextBox ID="InStock" runat="server"></asp:TextBox>
+                
+                <asp:Label ID="Label9" runat="server" Text="On Order" AssociatedControlID="OnOrder"></asp:Label>
+                <asp:TextBox ID="OnOrder" runat="server"></asp:TextBox>
+
+                <asp:Label ID="Label10" runat="server" Text="Reorder Level" AssociatedControlID="ReorderLevel"></asp:Label>
+                <asp:TextBox ID="ReorderLevel" runat="server"></asp:TextBox>
+
+                <asp:Label ID="Label11" runat="server" Text="Discontinued" AssociatedControlID="Discontinued"></asp:Label>
+                <asp:CheckBox ID="Discontinued" runat="server" Text="Product is discontinued"></asp:CheckBox>
             </fieldset>
         </div>
     </div>
+    <script src="../Scripts/bootwrap-freecode.js"></script>
 </asp:Content>
 
--- WebForms/Northwind Demo/Website/Purchasing/AddEditProduct.aspx.cs
+++ WebForms/Northwind Demo/Website/Purchasing/AddEditProduct.aspx.cs
@@ -14,23 +14,57 @@ public partial class Purchasing_AddEditProduct : System.Web.UI.Page
         if(!IsPostBack) // on the initial GET of the page
         {
             // We can populate some controls such as DropDownLists with data
-            // Populate CurrentProducts with all the products in the database
-            InventoryPurchasingController controller = new InventoryPurchasingController();
-            // controller is a NorthwindController object
-            List<Product> products = controller.ListAllProducts();
-            // products is a List<Product> object
-
-            CurrentProducts.DataSource = products;
-            // CurrentProducts is a DropDownList
-
-            CurrentProducts.DataTextField = "ProductName";
-            // The property of the Product class to display
-            CurrentProducts.DataValueField = "ProductID";
-            // The property of the Product class to use as the value
-
-            CurrentProducts.DataBind(); // Populate the DropDownList
-            CurrentProducts.Items.Insert(0, "[select a product]");
+            PopulateProductsDropDown();
+            PopulateSupplierDropDown();
+            PopulateCategoryDropDown();
         }
+    }
+
+    private void PopulateCategoryDropDown()
+    {
+        var controller = new InventoryPurchasingController();
+        var data = controller.ListAllCategories();
+        Category.DataSource = data;
+        Category.DataTextField = "CategoryName";
+        Category.DataValueField = "CategoryID";
+        Category.DataBind();
+        // Let's insert a couple of options at the top of the drop-down
+        Category.Items.Insert(0, new ListItem("[select a category]"));
+        Category.Items.Insert(1, new ListItem("[no category]", ""));
+        // The second inserted item is to accommodate a NULL value for the Product.CategoryID
+    }
+
+    private void PopulateSupplierDropDown()
+    {
+        InventoryPurchasingController controller = new InventoryPurchasingController();
+        List<Supplier> suppliers = controller.ListAllSupplier();
+        Supplier.DataSource = suppliers;
+        Supplier.DataTextField = "CompanyName";
+        Supplier.DataValueField = "SupplierID";
+        Supplier.DataBind();
+        // Let's insert a couple of options at the top of the drop-down
+        Supplier.Items.Insert(0, new ListItem("[select a supplier]"));
+        Supplier.Items.Insert(1, new ListItem("[no supplier]", "")); // because Product.SupplierID is nullable
+    }
+
+    private void PopulateProductsDropDown()
+    {
+        // Populate CurrentProducts with all the products in the database
+        InventoryPurchasingController controller = new InventoryPurchasingController();
+        // controller is a NorthwindController object
+        List<Product> products = controller.ListAllProducts();
+        // products is a List<Product> object
+
+        CurrentProducts.DataSource = products;
+        // CurrentProducts is a DropDownList
+
+        CurrentProducts.DataTextField = "ProductName";
+        // The property of the Product class to display
+        CurrentProducts.DataValueField = "ProductID";
+        // The property of the Product class to use as the value
+
+        CurrentProducts.DataBind(); // Populate the DropDownList
+        CurrentProducts.Items.Insert(0, "[select a product]");
     }
 
     protected void ShowProductDetails_Click(object sender, EventArgs e)
@@ -51,6 +85,17 @@ public partial class Purchasing_AddEditProduct : System.Web.UI.Page
                 // "Unpack" the data into the form's controls
                 ProductID.Text = item.ProductID.ToString(); // ProductID is an int
                 ProductName.Text = item.ProductName;
+                // Select the supplier/category for the found product
+                Supplier.SelectedValue = item.SupplierID.ToString();
+                Category.SelectedValue = item.CategoryID.ToString();
+                // Other values that are displayed in text boxes
+                QtyPerUnit.Text = item.QuantityPerUnit;
+                UnitPrice.Text = item.UnitPrice.ToString();
+                InStock.Text = item.UnitsInStock.ToString();
+                OnOrder.Text = item.UnitsOnOrder.ToString();
+                ReorderLevel.Text = item.ReorderLevel.ToString();
+                // Set the checkbox for the found product's Discontinued flag
+                Discontinued.Checked = item.Discontinued;
 
                 // Update the message
                 MessageLabel.Text = "Product details found.";
