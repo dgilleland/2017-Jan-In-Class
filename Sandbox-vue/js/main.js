f7793b842d4d4e9338baa29734c65151b02c68a7
Dan Gilleland <dgilleland@nait.ca>
2017-04-20 11:51:35 -0600

    Nesting components with bound values

--- Sandbox-vue/js/main.js
+++ Sandbox-vue/js/main.js
@@ -3,11 +3,34 @@ Vue.component('weighted-item', {
     template: 
     `<div>
       <input type="text" name="itemName" placeholder="Item Name" v-model="theName" />
-      <input type="number" name="weight" min="1" max="100" v-model="theWeight" /> ?
+      <input type="number" name="weight" min="1" max="100" v-model="theWeight" /> %
     </div>`,
     props: ['theName', 'theWeight']
 });
 
+
+// A Vue component that represents a set of markable items
+Vue.component('item-set', {
+    template: 
+    `<div>
+      <weighted-item v-for="item in items" :the-name="item.name" :the-weight="item.weight"></weighted-item>
+      <blockquote>
+        <weighted-item :the-name="itemToAdd.name" :the-weight="itemToAdd.weight"></weighted-item>
+        <button>Add</button>
+      </blockquote>
+    </div>`,
+    data: function() {
+        return {
+            items: [
+                { name: "Quiz 1", weight: 15 }
+            ],
+            itemToAdd: {
+                name: "", weight: null
+            }
+        }
+    }
+
+});
 
 
 // create the core/main Vue object
